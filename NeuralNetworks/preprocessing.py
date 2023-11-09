421ac1e70da18fef06126da707e5b291c720c609
B Steele <B.Steele@colostate.edu>
2023-11-09 14:18:34 -0700

    got super_overfit to actually overfit

--- NeuralNetworks/preprocessing.py
+++ NeuralNetworks/preprocessing.py
@@ -15,8 +15,6 @@ fn = os.path.join(file_path, "training_set_up_to_2021_v2023-11-08.csv")
 with open(fn) as f:
     df = pd.read_csv(f, sep=',')
 
-print(df.columns)
-
 # we need to preprocess these data according to their distribuions and ranges. 
 ### Precip data needs log transformation before standardization due to histo frequency.
 ### Wind data needs sqrt transormation.
@@ -28,11 +26,10 @@ precip = df.filter(like='precip')
 ### add 0.0001 to values in all columns of precip, since we need to log-transform the values and there are zeros
 precip += 0.0001
 precip = np.log(precip)
-print(precip.describe())
+
 ### and sqrt transform the wind vars
 wind = df.filter(like='wind')
 wind = np.sqrt(wind)
-print(wind.describe())
 
 # remove the precip and wind vars from df
 df = df.drop(columns=precip_vars)
@@ -40,8 +37,6 @@ df = df.drop(columns=wind_vars)
 # add the transformed precip and wind vars to df
 df = df.join(precip)
 df = df.join(wind)
-print(df.head)
-print(df.describe())
 
 # now, we'll standardize the data so that all the values are between -1 and 1.
 def standardize_column(df, col_name):
@@ -54,12 +49,9 @@ df_short = df.copy()
 df_short = df_short.drop(columns=['feature', 'date'])
 # and then apply the standardization function
 df_standardized = df_short.apply(lambda col: standardize_column(df_short, col.name))
-print(df_standardized.head)
-print(df_standardized.describe())
 
 # because we want to be able to apply these same standardizations to the test data, we should grab the mean/std values for each column
 df_mean_std = pd.DataFrame({'mean': df_short.mean(), 'std': df_short.std()})
-print(df_mean_std)
 
 # lets save this as a .csv file for later use
 # save df_mean_std to csv
@@ -123,51 +115,51 @@ val5 = training.loc[training['feature'] == 'Carter Lake']
 
 train6 = training.loc[training['feature'] != 'Willow Creek Reservoir']
 val6 = training.loc[training['feature'] == 'Willow Creek Reservoir']
-
-# and now save these all as pickle files
-def save_df_as_pickle(df, file_name, file_path=''):
-    """
-    Save a pandas dataframe as a pickle file.
-    
-    Parameters:
-    df (pandas.DataFrame): The dataframe to be saved.
-    file_name (str): The name of the file to be saved.
-    file_path (str): The path to the folder where the file will be saved.
-    
-    Returns:
-    None
-    """
-    with open(os.path.join(file_path, file_name), 'wb') as f:
-        pickle.dump(df, f)
-
-# list of dataframes to save as pickle files with file names
-list_ts_dfs = [[train1_ts, "train1_ts"],
-               [val1_ts, "val1_ts"],
-               [train2_ts, "train2_ts"],
-               [val2_ts, "val2_ts"],
-               [train3_ts, "train3_ts"],
-               [val3_ts, "val3_ts"],
-               [train4_ts, "train4_ts"],
-               [val4_ts, "val4_ts"]]
-
-list_dfs = [[train1, "train1"],
-            [val1, "val1"],
-            [train2, "train2"],
-            [val2, "val2"],
-            [train3, "train3"],
-            [val3, "val3"],
-            [train4, "train4"],
-            [val4, "val4"],
-            [train5, "train5"],
-            [val5, "val5"],
-            [train6, "train6"],
-            [val6, "val6"]]
-
-# map the function save_df_as_pickle across the list of dataframes list_ts_dfs and list_dfs
-for df, name in list_ts_dfs:
-    file_name = f"{name}.pkl"
-    save_df_as_pickle(df, file_name, "/Users/steeleb/OneDrive - Colostate/NASA-Northern/data/NN_train_val_test/pickles/")
-
-for df, name in list_dfs:
-    file_name = f"{name}.pkl"
-    save_df_as_pickle(df, file_name, "/Users/steeleb/OneDrive - Colostate/NASA-Northern/data/NN_train_val_test/pickles/")
+# 
+# # and now save these all as pickle files
+# def save_df_as_pickle(df, file_name, file_path=''):
+#     """
+#     Save a pandas dataframe as a pickle file.
+#     
+#     Parameters:
+#     df (pandas.DataFrame): The dataframe to be saved.
+#     file_name (str): The name of the file to be saved.
+#     file_path (str): The path to the folder where the file will be saved.
+#     
+#     Returns:
+#     None
+#     """
+#     with open(os.path.join(file_path, file_name), 'wb') as f:
+#         pickle.dump(df, f)
+# 
+# # list of dataframes to save as pickle files with file names
+# list_ts_dfs = [[train1_ts, "train1_ts"],
+#                [val1_ts, "val1_ts"],
+#                [train2_ts, "train2_ts"],
+#                [val2_ts, "val2_ts"],
+#                [train3_ts, "train3_ts"],
+#                [val3_ts, "val3_ts"],
+#                [train4_ts, "train4_ts"],
+#                [val4_ts, "val4_ts"]]
+# 
+# list_dfs = [[train1, "train1"],
+#             [val1, "val1"],
+#             [train2, "train2"],
+#             [val2, "val2"],
+#             [train3, "train3"],
+#             [val3, "val3"],
+#             [train4, "train4"],
+#             [val4, "val4"],
+#             [train5, "train5"],
+#             [val5, "val5"],
+#             [train6, "train6"],
+#             [val6, "val6"]]
+# 
+# # map the function save_df_as_pickle across the list of dataframes list_ts_dfs and list_dfs
+# for df, name in list_ts_dfs:
+#     file_name = f"{name}.pkl"
+#     save_df_as_pickle(df, file_name, "/Users/steeleb/OneDrive - Colostate/NASA-Northern/data/NN_train_val_test/pickles/")
+# 
+# for df, name in list_dfs:
+#     file_name = f"{name}.pkl"
+#     save_df_as_pickle(df, file_name, "/Users/steeleb/OneDrive - Colostate/NASA-Northern/data/NN_train_val_test/pickles/")
--- NeuralNetworks/settings.py
+++ NeuralNetworks/settings.py
@@ -1,13 +1,23 @@
 settings = {
   "basic" : { 
-    "hiddens": [10, 10, 10],
+    "hiddens": [20, 20, 20],
     "activations": ["relu", "relu", "relu"],
     "learning_rate": 0.001,
     "random_seed": 57,
     "max_epochs": 1000,
-    "batch_size": 32,
-    "patience": 50,
+    "batch_size": 64,
+    "patience": 200,
     "dropout_rate": 0
+  },
+  "super_overfit" : {
+    "hiddens": [20, 20, 20, 20, 20],
+    "activations": ["relu", "relu", "relu", "relu", "relu"],
+    "learning_rate": 0.001,
+    "random_seed": 57,
+    "max_epochs": 1000,
+    "batch_size": 128,
+    "patience": 500,
+    "dropout_rate": 0      
   },
   "with_dropout" : { 
     "hiddens": [10, 10, 10],
