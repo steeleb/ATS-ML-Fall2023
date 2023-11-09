706c6cb3bdb46bd104170beec3583989b319885d
B Steele <B.Steele@colostate.edu>
2023-11-09 15:59:42 -0700

    housecleaning

--- NeuralNetworks/settings.py
+++ NeuralNetworks/settings.py
@@ -1,11 +1,11 @@
 settings = {
   "basic" : { 
-    "hiddens": [20, 20, 20, 20, 20],
-    "activations": ["relu", "relu", "relu", "relu", "relu"],
+    "hiddens": [20, 20, 20, 20],
+    "activations": ["relu", "relu", "relu", "relu"],
     "learning_rate": 0.001,
     "random_seed": 57,
     "max_epochs": 1000,
-    "batch_size": 64,
+    "batch_size": 32,
     "patience": 200,
     "dropout_rate": 0
   },
